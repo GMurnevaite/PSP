04c8a23a5f4d3e57a5f3a09afdc9fd3006407acb
GMurnevaite <greta.murnevaite@gmail.com>
2021-09-13 21:56:10 +0300

    Initial commit

---
+++ Test/com/my/junit/EmailValidatorTest.java
@@ -0,0 +1,48 @@
+package com.my.junit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailValidatorTest {
+
+    @BeforeEach
+    void setUp() {
+        EmailValidator emailValidator = new EmailValidator();
+    }
+
+    //Test checks if the email has special symbol
+    @Test
+    void EmailValidator_EmailHasSpecialSymbol(){
+        assertTrue(emailValidator.specialSymbolRequirement("admin@gmail.com"));
+    }
+
+    //Test checks for invalid characters
+    @Test
+    void EmailValidator_NoUnsupportedCharacters(){
+        assertTrue(emailValidator.supportedCharactersRequirement("admin@gmail.com"));
+    }
+
+    @Test
+    void EmailValidator_HasUnsupportedCharacters(){
+        assertFalse(emailValidator.supportedCharactersRequirement("a¿dmin@gmail.com"));
+    }
+
+    //Test verifies that the mail has the correct domain and TLD
+    @Test
+    void EmailValidator_CorrectDomain(){
+        assertTrue(emailValidator.domainRequirement("admin@gmail.com"));
+    }
+
+    @Test
+    void EmailValidator_WrongDomain(){
+        assertFalse(emailValidator.domainRequirement("admin@email.1"));
+    }
+
+    @Test
+    void EmailValidator_WrongTLD(){
+        assertFalse(emailValidator.domainRequirement("admin@.lt"));
+    }
+}
---
+++ src/com/my/junit/PasswordChecker.java
@@ -0,0 +1,4 @@
+package com.my.junit;
+
+public class PasswordChecker {
+}
